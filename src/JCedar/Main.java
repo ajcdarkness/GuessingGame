fffa7b72c5bd063d7362e95f465adc9b4a27516b
Jacob <jcedar8640@bths.edu>
2018-09-27 11:34:40 -0400

    Fixed ai guessing

--- src/JCedar/Main.java
+++ src/JCedar/Main.java
@@ -23,35 +23,36 @@ public class Main {
         }
     }
 
-    public static void betterGuesses() {
+    public static void betterGuesses() { //Computer guesses and we answer "higher", "lower", or "correct"
         Scanner input = new Scanner(System.in);
+        int initial = 50;
+        System.out.println("50");
         int winCondition = 0;
-        int guessCount = 0;
-        int y = 100;
+        int guessCount = 1;
         while (winCondition == 0) {
-            System.out.println(y / 2);
-            guessCount++;
-            if (input.nextLine() == "higher") {
-                y = y / 2 + y / 4;
-                guessCount++;
-                System.out.println(y);
-                if(input.nextLine() == "higher"){
-                    y = y + y/8;
-                    guessCount++;
-                }else if(input.nextLine() == "lower"){
-                    y = y/2 + y/4;
-                }
-            } else if (input.nextLine() == "lower") {
-                y = y - y/4;
-                System.out.println(y);
-                guessCount++;
-                if(input.nextLine() == "higher"){
-                    y = y + y/8;
-                    guessCount++;
-                }
-            }else if(input.nextLine() == "correct")
-                winCondition = 1;
-
+            String UI = input.nextLine();
+           int num = 100/(guessCount*2 + 2); //Amount +- the curent guess is decided here
+           if(UI.equals("higher")){
+               if(initial < 90) { //if num is greater then 90 it was throwing an error so modified the code.
+                   initial = initial + num; //adds to the number about to be guessed based upon the amount of guesses made and the previous guess
+                   System.out.println(initial);
+               }else{ // greater then 90 iterate by 1
+                   initial = initial + 1;
+                   System.out.println(initial);
+               }
+           }else if(UI.equals("lower")){
+               if(initial >10) { ////if num less then 10 it was throwing an error so modified the code.
+                   initial = initial - num; //subtracts to the number about to be guessed based upon the amount of guesses made and the previous guess.
+                   System.out.println(initial);
+               }else{ // less then 10 iterate by 1
+                   initial = initial - 1;
+                   System.out.println(initial);
+               }
+           }else if(UI.equals("correct")){ //if the guess is correct, break the while loop and computer states that it won
+               System.out.println("I have guessed correctly");
+               winCondition = 1;
+           }
+           guessCount++;
         }
         System.out.println("done");
     }
